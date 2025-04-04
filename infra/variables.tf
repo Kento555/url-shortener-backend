d9fa5aa9d66f9d13237a3a545f8cfa786ae35fc9
Kento555 <shen_kento555@hotmail.com>
2025-04-04 16:54:55 +0800

    update yaml

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,8 @@ variable "route53_zone_id" {
   description = "Route53 Hosted Zone ID"
   type        = string
 }
+
+variable "allowed_ips" {
+  description = "List of allowed IP addresses for WAF"
+  type        = list(string)
+}
